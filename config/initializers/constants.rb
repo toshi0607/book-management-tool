ca53d822ddc05821b62cc4a76969ab063edfd0f4
Tshinori Sugita <sugita@c-fo.com>
2015-02-22 22:55:34 +0900

    use constants

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,6 @@
 module Constants
+  # RAKUTEN_API = "/services/api/BooksBook/Search/20130522"
+  # AP_ID = '1062746000989149114'
   if Rails.env.production? || Rails.env.staging?
     CA_FILE = '/usr/lib/ssl/certs/ca-certificates.crt'
   else
